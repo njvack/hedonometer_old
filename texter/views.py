7b411ac9a31eace623932c5598b704cc85f12a5c
Nate Vack <njvack@freshforever.net>
2011-12-09 16:25:48 -0600

    Silly trivial tropo-only response test.

--- texter/views.py
+++ texter/views.py
@@ -1,12 +1,19 @@
+
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 import logging
 logger = logging.getLogger('texter')
 
+import tropo # Temporary! Abstract this.
+
 class IncomingView(TemplateView):
-    template_name = 'incoming.html'
     
     def dispatch(self, *args, **kwargs):
         logger.debug('This is IncomingView#dispatch()')
-        return super(IncomingView, self).dispatch(*args, **kwargs)
-        
+        resp = HttpResponse(content_type='application/json')
+        t = tropo.Tropo()
+        t.say('This is a response')
+        resp.write(t.RenderJson())
+        return resp
+
