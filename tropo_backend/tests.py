c5303f6cb4c067d1bfc18a141440070b85fc6cee
Nate Vack <njvack@freshforever.net>
2011-12-19 13:55:32 -0600

    Add copyright notice to tropo_backend app

--- tropo_backend/tests.py
+++ tropo_backend/tests.py
@@ -1,3 +1,8 @@
+# coding: utf8
+# Part of hedonometer -- a text-messing-based experience sampler
+#
+# Copyright (c) 2011 Board of Regents of the University of Wisconsin System
+
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -9,6 +14,7 @@ from django.test import TestCase
 
 
 class SimpleTest(TestCase):
+
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
