04255fcfa9ef5c8d7000ffff17b0188705807641
Nate Vack <njvack@freshforever.net>
2012-02-03 14:40:28 -0600

    Add a bunch of stuff to the admin.

--- texter/admin.py
+++ texter/admin.py
@@ -7,17 +7,65 @@ from django.contrib import admin
 from . import models
 
 
+class QuestionPartInline(admin.TabularInline):
+
+    model = models.QuestionPart
+
+    extra = 1
+
+
+class ParticipantInline(admin.TabularInline):
+
+    model = models.Participant
+
+    extra = 1
+
+
+class TaskDayInline(admin.TabularInline):
+
+    model = models.TaskDay
+
+    extra = 1
+
+
+class ScheduledSampleInline(admin.TabularInline):
+
+    model = models.ScheduledSample
+
+    extra = 1
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+
+    inlines = [
+        TaskDayInline,
+        ScheduledSampleInline]
+
+
 class ExperimentAdmin(admin.ModelAdmin):
 
     readonly_fields = ['url_slug', ]
+
+    inlines = [
+        QuestionPartInline,
+        ParticipantInline]
 
 
 class BackendAdmin(admin.ModelAdmin):
 
     readonly_fields = ['delegate_classname', 'delegate_pk', 'name']
 
+
+class TextMessageAdmin(admin.ModelAdmin):
+
+    pass
+
 admin.site.register(models.Experiment, ExperimentAdmin)
 
 admin.site.register(models.Backend, BackendAdmin)
 
-admin.site.register(models.Participant, admin.ModelAdmin)
+admin.site.register(models.Participant, ParticipantAdmin)
+
+admin.site.register(models.IncomingTextMessage, TextMessageAdmin)
+
+admin.site.register(models.OutgoingTextMessage, TextMessageAdmin)
